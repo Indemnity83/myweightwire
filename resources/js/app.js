4829948e0583c7494d9428f1a97cc648db9b58be
Kyle Klaus <kklaus@indemnity83.com>
2019-01-06 23:29:22 -0800

    fix: make hamburger functional

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,6 +21,7 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('line-chart', require('./components/LineChart.vue').default);
+Vue.component('navigation', require('./components/Navigation.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
